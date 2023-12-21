86569c32286d9f3d073c218a803d1f68bf19027a
Vellethe <95373802+Vellethe@users.noreply.github.com>
2023-12-21 12:59:48 +0100

    Add files via upload

---
+++ Backend1/Backend1/Data/Context.cs
@@ -0,0 +1,15 @@
+﻿using Backend1.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Backend.Data
+{
+    public class TodoContext : DbContext
+    {
+        public TodoContext(DbContextOptions<TodoContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Note> Notes { get; set; }
+    }
+}
