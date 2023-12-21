2782f4c2ece7174a8259dae2c313fbec5ba2c201
Vellethe <95373802+Vellethe@users.noreply.github.com>
2023-12-21 12:41:08 +0100

    .

---
+++ Todo/Data/Context.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+using Todo.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Todo.Data
+{
+    public class TodoContext : DbContext
+    {
+        public TodoContext(DbContextOptions<TodoContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Note> Notes { get; set; }
+    }
+}
