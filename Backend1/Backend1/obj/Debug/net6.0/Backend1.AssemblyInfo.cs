48d2c8882ca2c26ae83375122021c698e3914767
Vellethe <95373802+Vellethe@users.noreply.github.com>
2023-12-22 12:50:37 +0100

    .

--- Backend1/Backend1/obj/Debug/net6.0/Backend1.AssemblyInfo.cs
+++ Backend1/Backend1/obj/Debug/net6.0/Backend1.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Backend1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e86a5dc5d85c4a472af4c21776c84fe77976fad5")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("Backend1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Backend1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
